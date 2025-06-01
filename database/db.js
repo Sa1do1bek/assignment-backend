1be0c8947ee2180eccf579ecae8555bfe42ec6b2
Sardor <sardo1be1@gmail.com>
2025-06-01 23:56:09 +0500

    save

--- database/db.js
+++ database/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URL);
+        const conn = await mongoose.connect('mongodb+srv://sardo1be1:aTZBGgj0sME7dHiQ@cluster0.ji6tr52.mongodb.net/support?retryWrites=true&w=majority&appName=Cluster0');
         console.log(`MongoDB connected: ${conn.connection.host}`);
         
     } catch (error) {
